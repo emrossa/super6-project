674ad9e2b76547cc6387b6f397f6326e7cf8b785
Emilia Lewandowska <emilia.lewandowska2@skybettingandgaming.com>
2018-10-01 09:06:11 +0100

    WIP

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        if (username == 'emilia' && password == '1212') {
+            return done(null, {username, password});
+        }
+
+        return done(null, false, {message: 'Incorrect details'});
+    }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.set('view engine', 'hbs')
+
+app.get('/', function (req, res) {
+    res.send('Hello World! ' + JSON.stringify(req.user));
+})
+
+app.get('/login', function (req, res) {
+    res.render('login');
+})
+
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
